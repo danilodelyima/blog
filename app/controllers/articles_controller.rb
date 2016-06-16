04eda45f512945f27504447deaf8058d12ba8198
danilo <danilocabrinha@cjr.org.br>
2016-06-16 13:52:34 -0300

    fazendo o update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,28 @@
 class ArticlesController < ApplicationController
 	def index
-		@articles = Article.all
-
+		#Recebe todo o conteúdo do banco de dados e armazena em @articles
+		@articles = Article.all 
 	end
 
 	def show
-		@articles = Article.find(params[:id])
+		#Salva em @articles o objeto com o parâmetro
+		@article = Article.find(params[:id])  
 	end
 
 	def new
-		@article = Article.new
+		# Obtém a própria página. 
+		# GET
+		@article = Article.new 
 	end
 
+	def edit
+		@article = Article.find(params[:id])  
+	end	
+
 	def create
+		# Recebe os inputs do form (params), então, salva se as condições forem verdadeiras,
+		# se não renderiza a página mostrando o(s) erro(s).
+		# POST
 		@article = Article.new article_params
 		if @article.save
 			redirect_to articles_path
@@ -21,9 +31,19 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to article_path(@article.id)
+		else
+			render 'edit'
+		end
+	end
+
 	private
 	def article_params
-		params.require(:article).permit(:title, :text)#Limitando os campos dos quais o user pode submeter
+		#Limita os campos dos quais o user pode submeter,
 		#O método permit, retorna um hash com o que você permitiu
+		params.require(:article).permit(:title, :text)
 	end
 end
