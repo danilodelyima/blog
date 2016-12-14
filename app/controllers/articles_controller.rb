a1ddcfa621d69c0fc6e8d8fd31c5f157e0268627
Danilo de Lima <danilodelyima@gmail.com>
2016-12-14 16:38:56 -0200

    refatorando código

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,22 @@
 class ArticlesController < ApplicationController
+
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+	#Recebe todo os artigos do banco de dados e armazena em @articles
 	def index
-		#Recebe todo o conteúdo do banco de dados e armazena em @articles
 		@articles = Article.all 
 	end
 
-	def show
-		#Salva em @articles o objeto com o parâmetro
-		@article = Article.find(params[:id])  
-	end
-
-	def new
-		# Obtém a própria página. 
-		# GET
+	# GET
+	def new 
 		@article = Article.new 
 	end
 
-	def edit
-		@article = Article.find(params[:id])  
-	end	
-
+	# Recebe os inputs do form (params), então, salva se as condições forem verdadeiras,
+	# se não renderiza a página mostrando o(s) erro(s).
+	# POST
 	def create
-		# Recebe os inputs do form (params), então, salva se as condições forem verdadeiras,
-		# se não renderiza a página mostrando o(s) erro(s).
-		# POST
+		#render plain: params
 		@article = Article.new article_params
 		if @article.save
 			redirect_to articles_path
@@ -31,6 +25,15 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+
+	#Salva em @articles o objeto com o parâmetro
+	def show  
+	end
+
+	def edit 
+	end	
+
+	
 	def update
 		@article = Article.find(params[:id])
 		if @article.update(article_params)
@@ -41,17 +44,20 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
-
 		redirect_to articles_path
-
 	end
 
 	private
+
+	#Limita os campos dos quais o user pode submeter,
+	#O método permit, retorna um hash com o que você permitiu
 	def article_params
-		#Limita os campos dos quais o user pode submeter,
-		#O método permit, retorna um hash com o que você permitiu
 		params.require(:article).permit(:title, :text)
 	end
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
 end
