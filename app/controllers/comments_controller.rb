082a49b683e777418f0e121dd05684dce2c87c9d
Danilo de Lima <danilodelyima@gmail.com>
2016-12-14 19:02:26 -0200

    artigos com comentários podem ser deletados

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
 	def create
-		# Recupera o article específico(ID)
+		# Recupera o article específico pelo id
 		@article = Article.find(params[:article_id])
-		# 
-		@comment = @article.comments.create comment_params
-		# Redireciona para o artigo específico(ID)
-		redirect_to article_path(@article.id)
+		# Usa relação artigo x comentário e cria um comentário no artigo específico 
+		@article.comments.create comment_params
+		# Redireciona para o artigo específico 
+		redirect_to article_path(@article)
 	end
 
 	private
 	def comment_params
-		#:comment é uma chave que tem como valor os inputs do form
+		#:comment é uma chave do hash params que tem como valor os inputs do form
 		params.require(:comment).permit(:commenter, :body)
 	end
 end
